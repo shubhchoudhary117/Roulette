70c88731582cd2a4c30d3e6f97150f51e4f9aa42
shubham <miraigategroup@gmail.com>
2023-12-18 23:55:57 -0100

    changes in database uri

--- db/connection.js
+++ db/connection.js
@@ -1,6 +1,6 @@
 const mongoose=require("mongoose");
 const connect=async()=>{
-   await mongoose.connect("mongodb://127.0.0.1:27017/Roulette", { useNewUrlParser: true, useUnifiedTopology: true })
+   await mongoose.connect("mongodb+srv://shafisadique123:shafisadique123@cluster0.brlacya.mongodb.net/Roulatte?ssl=true&tls=true", { useNewUrlParser: true, useUnifiedTopology: true })
     .then(()=>{
         console.log(`database connected successfully`)
     })
